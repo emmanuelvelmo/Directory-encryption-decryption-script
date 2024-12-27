12ef1b240ec94bb30f6501914b883ceea1fb8cd6
Emmanuel V. <51292782+emmanuelvelmo@users.noreply.github.com>
2024-12-27 11:34:35 -0800

    setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 from cx_Freeze import setup, Executable
 
-setup(name="Directory encryption decryption", executables=[Executable("Directory encryption decryption script.py")])
+setup(name="Directory encryption decryption", executables=[Executable("Directory encryption decryption script.py")], options={"build_exe": {"excludes": ["tkinter"]}})
