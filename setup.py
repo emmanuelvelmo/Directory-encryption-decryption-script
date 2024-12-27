d853c9c30148c4fa7b7ae22ec9be57d57a3b3951
Emmanuel V. <51292782+emmanuelvelmo@users.noreply.github.com>
2024-12-27 09:33:39 -0800

    setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,3 @@
 from cx_Freeze import setup, Executable
 
-setup(
-    name="Directory encryption decryption",
-    version="1.0",
-    description="Directory encryption script",
-    executables=[Executable("Directory encryption decryption script.py")]
-)
+setup(name="Directory encryption decryption", executables=[Executable("Directory encryption decryption script.py")])
